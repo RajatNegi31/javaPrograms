05e7f03e10ef4dffb970d940730740774c64dca8
RajatNegi31 <rajat.negi67@gmail.com>
2024-03-10 15:54:48 +0530

    m

--- array1.java
+++ array1.java
@@ -2,12 +2,18 @@ public class array1 {
     
     public static void main(String[] args){
 
+        print_array();
+
+
+
+    }
+
+    public static void print_array(){
+
         int arr1[]={1,2,3,4,5,6,7,8,9};
         for(int x : arr1){
             System.out.println(x);
         }
-
-
 
     }
 }
