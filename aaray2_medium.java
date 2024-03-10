60d863fbecf672295e7168dcc5f132fc134ad4cb
RajatNegi31 <rajat.negi67@gmail.com>
2024-03-10 20:12:01 +0530

    m

---
+++ aaray2_medium.java
@@ -0,0 +1,17 @@
+public class aaray2_medium {
+    
+    public static void main(String[] args){
+
+
+
+    }
+
+    public  static void freq_count(){
+
+        int arr1[]={11,22,33,44,55,66,77,88,88,99,99,22,3344,44,44,33,66,7788,88,88,889,99,998,88,77};
+        
+
+    }
+
+
+}
